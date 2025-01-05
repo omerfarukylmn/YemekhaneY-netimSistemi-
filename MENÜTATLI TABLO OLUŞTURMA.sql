af66d73cf8fa9d67633fac26fb847c88d944ffee
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:48:53 +0300

    Delete MENÜTATLI TABLO OLUŞTURMA.sql

--- MENÜTATLI TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table MenuTatli(
-menuId INT,
-tatliId INT,
-PRIMARY KEY (menuId, tatliId),
-FOREIGN KEY (menuId) REFERENCES Menu(menuId),
-FOREIGN KEY (tatliId) REFERENCES Tatli(tatliId),
-);
