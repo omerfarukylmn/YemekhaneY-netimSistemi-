42ae23b9c116b7445d89c556758c45ea006dfdf3
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:48:32 +0300

    Delete MENÜİÇECEK TABLO OLUŞTURMA.sql

--- MENÜİÇECEK TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table MenuIcecek(
-menuId INT,
-icecekId INT,
-PRIMARY KEY (menuId, icecekId),
-FOREIGN KEY (menuId) REFERENCES Menu(menuId),
-FOREIGN KEY (icecekId) REFERENCES Icecek(icecekId),
-);
