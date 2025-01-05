850f4e80110b309ca1f208455e175526e8a840f9
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:47:18 +0300

    Delete ÖĞRENCİ TABLO OLUŞTURMA.sql

--- ÖĞRENCİ TABLO OLUŞTURMA.sql
+++
@@ -1,9 +0,0 @@
-Create Table Ogrenci(
-ogrenciNo INT PRIMARY KEY,
-bolum NVARCHAR(50),
-fakulte NVARCHAR(50),
-sinif INT,
-iskontoOrani FLOAT,
-kisiId INT,
-FOREIGN KEY(kisiId) REFERENCES Kisi(kisiId)
-);
