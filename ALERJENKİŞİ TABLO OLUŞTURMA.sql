6b221b0c09e3adf23430dd5c6ae3aa8c95242050
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:46:33 +0300

    Delete ALERJENKİŞİ TABLO OLUŞTURMA.sql

--- ALERJENKİŞİ TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table AlerjenKisi(
-alerjenId INT,
-kisiId INT,
-PRIMARY KEY (alerjenId, kisiId ),
-FOREIGN KEY (alerjenId) REFERENCES Alerjen(alerjenId),
-FOREIGN KEY (kisiId ) REFERENCES Kisi(kisiId ),
-);
