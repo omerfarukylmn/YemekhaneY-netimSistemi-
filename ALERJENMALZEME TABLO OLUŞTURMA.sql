d3328b1ca6feac23943a35d50ef089fff41a9f18
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:46:43 +0300

    Delete ALERJENMALZEME TABLO OLUŞTURMA.sql

--- ALERJENMALZEME TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table AlerjenMalzeme(
-alerjenId INT,
-malzemeId INT,
-PRIMARY KEY (alerjenId, malzemeId),
-FOREIGN KEY (alerjenId) REFERENCES Alerjen(alerjenId),
-FOREIGN KEY (malzemeId) REFERENCES Malzeme(malzemeId),
-);
