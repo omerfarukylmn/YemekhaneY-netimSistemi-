4917f50b737afdfc2295a40842a5ba501910874b
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:47:34 +0300

    Delete YEMEKMALZEME TABLO OLUŞTURMA.sql

--- YEMEKMALZEME TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table YemekMalzeme(
-yemekId INT,
-malzemeId INT,
-PRIMARY KEY (yemekId, malzemeId),
-FOREIGN KEY (yemekId) REFERENCES Yemek(yemekId),
-FOREIGN KEY (malzemeId) REFERENCES Malzeme(malzemeId),
-);
