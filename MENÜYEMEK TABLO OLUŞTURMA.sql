da6b86c7a125da36c456b9f06e31c7fa8af60614
ÖMER FARUK YELMAN <121029378+omerfarukylmn@users.noreply.github.com>
2025-01-05 17:48:41 +0300

    Delete MENÜYEMEK TABLO OLUŞTURMA.sql

--- MENÜYEMEK TABLO OLUŞTURMA.sql
+++
@@ -1,7 +0,0 @@
-Create Table MenuYemek(
-menuId INT,
-yemekId INT,
-PRIMARY KEY (menuId, yemekId),
-FOREIGN KEY (menuId) REFERENCES Menu(menuId),
-FOREIGN KEY (yemekId) REFERENCES Yemek(yemekId),
-);
